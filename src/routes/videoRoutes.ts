8cc56e649b87ef23e65fc83f375adfaddae3e948
p6974875-cell <p6974875@gmail.com>
2025-09-03 10:36:24 +0000

    Delete src directory

--- src/routes/videoRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generateVideo } from "../controllers/videoController";
-
-const router = Router();
-
-router.post("/generate", generateVideo);
-
-export default router;
